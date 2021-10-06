372d477283852ab42b9d01ad79892908abf19a29
Ale <akip-core@outlook.com>
2021-10-06 13:00:18 +0200

    cose

--- app/src/main/java/com/example/ex1g8constrains/MainActivity.kt
+++ app/src/main/java/com/example/ex1g8constrains/MainActivity.kt
@@ -3,16 +3,23 @@ package com.example.ex1g8constrains
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     lateinit var stellaMerda:FloatingActionButton
+    lateinit var campoTesto:EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.constrain_layout)
 
         stellaMerda = findViewById(R.id.stellaDemmmerda)
+        campoTesto = findViewById(R.id.editTextTextPersonName)
+
+        stellaMerda.setOnClickListener{
+            campoTesto.setText("Hai premuto la stella demmerda")
+        }
 
 
 
