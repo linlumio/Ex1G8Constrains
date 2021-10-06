67eccd7fed6fe34fb4dc855b797b073e25ee5c47
Ale <akip-core@outlook.com>
2021-10-06 12:32:39 +0200

    mi fermo alla stelllademmmmerda

--- app/src/main/java/com/example/ex1g8constrains/MainActivity.kt
+++ app/src/main/java/com/example/ex1g8constrains/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.ex1g8constrains
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var stellaMerda:FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.constrain_layout)
+
+        stellaMerda = findViewById(R.id.stellaDemmmerda)
+
+
+
     }
+
+
 }
